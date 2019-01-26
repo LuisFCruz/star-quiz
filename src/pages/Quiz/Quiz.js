6db7caed606fc1b9e9d2f0617169989860ce4186
Luis Cruz <luis.cruz@outlook.com.br>
2019-01-26 16:14:27 -0200

    [add] implementação da busca dos complementos

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -2,7 +2,8 @@ import { withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import CardCharacter from '../../components/CardCharacter/CardCharacter';
-import { getCharacters } from '../../services/starwars-api';
+import { getCharacters, getPlanetName, getVehicles, getSpecies, getFilms } from '../../services/starwars-api';
+import Modal from '../../components/Modal/Modal';
 
 const styles = {
   grid: {
@@ -16,7 +17,11 @@ class Quiz extends Component {
   constructor() {
     super();
     this.state = {
-      characters: []
+      characters: [],
+      modal: {
+        open: false,
+        character: null
+      }
     }
     this.characters = [];
   }
@@ -26,16 +31,46 @@ class Quiz extends Component {
     this.setState({ characters });
   }
 
+  handleModalClickOpen = async (character) => {
+    const {
+      species: speciesUrl,
+      vehicles: vehiclesUrl,
+      homeworld: homeworldUrl,
+      films: filmsUrls,
+    } = character;
+
+    const species = await getSpecies(speciesUrl);
+    const vehicles = await getVehicles(vehiclesUrl);
+    const homeworld = await getPlanetName(homeworldUrl);
+    const films = await getFilms(filmsUrls);
+
+    const modal = { open: true, character: {...character, homeworld, species, vehicles, films} }
+    this.setState({ modal });
+  };
+
+  handleModalClose = () => {
+    const modal = { open: false, character: null };
+    this.setState({ modal });
+  };
+
   render() {
     const { classes } = this.props
-    const { characters } = this.state;
+    const { characters, modal } = this.state;
     return (
       <div className={classes.grid}>
         {
           characters.map((character, index) =>
-            <CardCharacter {...character} key={index}/>
+            <CardCharacter
+              character={character}
+              key={index}
+              onHelpClick={this.handleModalClickOpen}
+            />
           )
         }
+        <Modal
+          {...modal}
+          onClose={this.handleModalClose}
+        />
       </div>
     )
   }
--- src/services/starwars-api.js
+++ src/services/starwars-api.js
@@ -8,10 +8,31 @@ export const getCharacters = async (page = 1) => {
   return parseCharacters(data.results);
 }
 
-
 const parseCharacters = (characters) => {
   return characters.map(character => {
     const id = character.url.replace(/[^\d]+/g, "")
     return {...character, id };
   });
+}
+
+export const getFilms = async (urls) => {
+  return getComplements(urls, 'title');
+}
+
+export const getVehicles = async (urls) => {
+  return getComplements(urls, 'name');
+}
+
+export const getSpecies = async (urls) => {
+  return getComplements(urls, 'name');
+}
+
+export const getPlanetName = async (url) => {
+  const { data } = await axios.get(url);
+  return data.name;
+}
+
+const getComplements = async (urls, attr) => {
+  const complement = await Promise.all(urls.map(url => axios.get(url)));
+  return complement.map(({data}) => data[attr]).join(", ");
 }
