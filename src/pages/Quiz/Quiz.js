7c6ee5b6ae1117b65351519672d33770c75a599a
Luis Cruz <luis.cruz@outlook.com.br>
2019-01-26 18:26:05 -0200

    [add] criação do componente de cronometro

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import CardCharacter from '../../components/CardCharacter/CardCharacter';
 import { getCharacters, getPlanetName, getVehicles, getSpecies, getFilms } from '../../services/starwars-api';
 import Modal from '../../components/Modal/Modal';
+import Timer from '../../components/Timer/Timer';
 
 const styles = {
   grid: {
@@ -21,14 +22,20 @@ class Quiz extends Component {
       modal: {
         open: false,
         character: null
-      }
+      },
+      startTime: false
     }
     this.characters = [];
+  }
+
+  setModal(open, character) {
+    const modal = { open, character };
+    this.setState({ modal });
   }
   
   async componentWillMount() {
     const characters =  await getCharacters(1);
-    this.setState({ characters });
+    this.setState({ characters, startTime: true });
   }
 
   handleModalClickOpen = async (character) => {
@@ -44,20 +51,20 @@ class Quiz extends Component {
     const homeworld = await getPlanetName(homeworldUrl);
     const films = await getFilms(filmsUrls);
 
-    const modal = { open: true, character: {...character, homeworld, species, vehicles, films} }
-    this.setState({ modal });
+    this.setModal(true, {...character, homeworld, species, vehicles, films})
   };
 
   handleModalClose = () => {
-    const modal = { open: false, character: null };
-    this.setState({ modal });
+    this.setModal(false, null);
   };
 
   render() {
+    const time = 2;
     const { classes } = this.props
-    const { characters, modal } = this.state;
+    const { characters, modal, startTime } = this.state;
     return (
       <div className={classes.grid}>
+        <Timer time={time} start={startTime} />
         {
           characters.map((character, index) =>
             <CardCharacter
