e008d5af45c0dd9e56d0295a4e3058ebcefae88d
Luis Cruz <luis.cruz@invillia.com>
2019-02-14 15:03:32 -0200

    [add] adução do testes da modal

---
+++ src/components/Modal/Modal.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'enzyme';
+
+import { Modal } from './Modal'
+
+describe('<Modal />', () => {
+    const character = {
+        id: '1',
+        height: '178',
+        hairColor: 'red',
+        films: 'star wars',
+        species: 'human',
+        homeworld: 'earth',
+        vehicles: 'car',
+    }
+  const props = { character, classes: {} }
+  const wrapper = render(<Modal { ...props } />)
+
+  test('render', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
